7bc6bdebc95bf9b0d89ba4294d3bdb52a1e78032
mugunthan638 <mugunthanswetha@gmail.com>
2024-09-12 12:58:05 +0530

    Testing

--- src/org/abc/Aabbcc.java
+++ src/org/abc/Aabbcc.java
@@ -1,5 +1,13 @@
 package org.abc;
 
 public class Aabbcc {
+	
+	public static void main(String[] args) {
+		int a = 7;
+		
+		System.out.println("Start");
+		
+		System.out.println("Ended");
+	}
 
 }
